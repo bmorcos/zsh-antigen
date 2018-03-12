6554f4b63bbe1ecdba8a2accc4298a9e0dccaa75
bmorcos <morcos.ben@gmail.com>
2018-03-12 17:30:42 -0400

    remove script to reorder panel icons

--- fix_icons.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-sudo sed -e "s/dropbox-client-.*=/dropbox-client-$(ps aux | grep dropbox | grep dist | awk '{print $2 }')=/g" -i /usr/share/indicator-application/ordering-override.keyfile
-restart unity-panel-service
